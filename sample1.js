170c8104c02b6549576fd07f433af41b0f51e681
yamakit <18439@toba.kosen-ac.jp>
2021-01-24 15:14:12 +0900

    ページ全体でマウスポインタのY座標を取得する

--- sample1.js
+++ sample1.js
@@ -1,17 +1,13 @@
 window.onload=function(){
-    //マウス移動時のイベントをBODYタグに登録する
-    document.body.addEventListener("mousemove", function(e){
-    var Interval;
+  //マウス移動時のイベントをBODYタグに登録する
+  document.body.addEventListener("mousemove", function(e){
+ 
     //座標を取得する
-    function get(){
-      var mY = e.pageY;  //Y座標
-    }
-
-    function interval(){
-        Interval = setInterval(get,1000);
-    }
-    //座標を表示する 
-    document.getElementById("txtY").value = Interval;
-    });
-  }
-  
+    var mX = e.pageX;  //X座標
+    var mY = e.pageY;  //Y座標
+ 
+    //座標を表示する
+    document.getElementById("txtX").value = mX;
+    document.getElementById("txtY").value = mY;
+  });
+}
