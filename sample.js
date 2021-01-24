fb43d36572777be8021b051c12b373d477d719a9
yamakit <18439@toba.kosen-ac.jp>
2021-01-24 14:57:49 +0900

    ボタンを押すとマウスポインタのY座標を表示

--- sample.js
+++ sample.js
@@ -1,37 +1,12 @@
-// window.onload=function(){
-//   var Interval;
-//   //マウス移動時のイベントをBODYタグに登録する
-// function get(){
-//   var mY = e.pageY;
-// }
-
-// function interval(){
-//   Interval = setInterval(get,1000);
-// }
-
-//  setInterval(function(){
-  
-//     //座標を表示する
-//     document.getElementById("txtY").value = Interval;
-//   });
-//  },1000);
-// }
-
-window.onload=function(){
-  //マウス移動時のイベントをBODYタグに登録する
-
-  var mmY = function(){
-    document.body.addEventListener("mousemove", function(e){
-    const log = function(){
-      var mY = e.pageY;  //Y座標
-    }
-    console.log(log);
-    // //座標を表示する 
-    // document.getElementById("txtY").value = Interval;
-    });
+var mY = 0;
+window.onload = function(){
+  document.body.addEventListener("mousemove", function(e){
+  mY = e.pageY;
+  console.log(mY);
+  });
 }
-var a = setInterval(mmY,1000);
-console.log(a);
-document.getElementById("txtY").value = a;
 
+function mouse(){
+  console.log(mY);
+  document.getElementById("mouse").innerHTML = "Y座標：" + mY;
 }
